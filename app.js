5adbaea8366d6f85d9d20580388ec37f375c72c0
Helena Saminez <helenasaminez@gmail.com>
2025-11-26 21:45:00 -0300

    feat: :art: Melhoria na rota login e correção nas views login e home

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
 // Importação dos pacotes
+
+const loginRouter = require("./routes/login");
+
 // Instância para o Express
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
 
-// Rotas
-const loginRoutes = require("./routes/login")
-
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3001;
 
 // view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-// Configurações
+// middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(session({
-    secret: 'troque-por-uma-chave-secreta',
-    resave: false,  
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "troque-por-uma-chave-secreta",
+    resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 }
-}));
-
-// Servir arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+    cookie: { maxAge: 1000 * 60 * 60 },
+  })
+);
 
 // Confguração dos middlewares
 
 // Importação das rotas
-app.use("/", loginRoutes);
-
-// Exemplo de próximas rotas:
-// app.use("/", userRoutes);
-
-
+app.use(loginRouter);
 // Execução do servidor
-
-// Exportando o app para o server.js
-module.exports = app;
+try {
+  app.listen(PORT);
+  console.log(`Running on http://locahost:${PORT}`);
+} catch {
+  console.log("Error servidor não pode ser iniciado.");
+}
